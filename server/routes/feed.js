e53c32859cb1933cfb4a580924467efb1b93e26b
tfrere <tfrere@student.42.fr>
2017-08-31 22:01:44 +0200

    fix rss-to-json bug

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -19,27 +19,34 @@ module.exports = {
     }
     else {
       context.app.get('/feed', function(req,res) {
+
         console.log("route");
+
         Feed.load(req.param("url"), function(err, rss) {
-          console.log("length", rss.items.length);
           let feed = [];
-          rss.items.map(function(item, index) {
-            feed.push({
-              title: item.title,
-              content: tools.sanitizeContent(item.description),
-              image: tools.firstImageUrl(item.description),
-              date: moment(item.created).fromNow(),
-              isFreshContent: tools.isFreshContent(item.created),
-              link: item.link,
-              creator: item.creator,
-              status: "success"
+          if (rss && rss.items) {
+            console.log("length", rss.items.length);
+            rss.items.map(function(item, index) {
+              feed.push({
+                title: item.title,
+                content: tools.sanitizeContent(item.description),
+                image: tools.firstImageUrl(item.description),
+                date: moment(item.created).fromNow(),
+                isFreshContent: tools.isFreshContent(item.created),
+                link: item.link,
+                creator: item.creator,
+                status: "success"
+              });
+              console.log(rss.items.length, index);
+              if (rss.items.length == index + 1) {
+                console.log(1);
+                res.status(200).send(feed);
+              }
             });
-            console.log(rss.items.length, index);
-            if (rss.items.length == index + 1) {
-              console.log(1);
-              res.status(200).send(feed);
-            }
-          });
+          }
+          else {
+            res.status(500).send("ça marche pas");
+          }
         });
 
       });
